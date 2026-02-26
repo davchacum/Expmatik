8b9254ab0f04471c7f0ddeac858a16ee60f34ae2
davchacum <davchacum@alum.us.es>
2026-02-26 18:55:28 +0100

    feat: inicialización de frontend y backend

---
+++ backend/src/main/java/com/vending/backend/VendingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vending.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vending/backend/VendingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vending.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
